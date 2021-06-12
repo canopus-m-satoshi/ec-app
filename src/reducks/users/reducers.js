cfc59ab11cf57d8df69ef880938eb149247e21ae
Satoshi Matsushima <>
2021-06-12 11:42:20 +0900

    initial commit

---
+++ src/reducks/users/reducers.js
@@ -0,0 +1,20 @@
+import * as Actions from './actions';
+import initialState from '../store/initialState';
+
+export const UserReducer = (state = initialState.users, action) => {
+  switch (action.type) {
+    case Actions.SIGN_IN:
+      return {
+        ...state,
+        ...action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const payload = {
+  uid: '000000',
+  username: 'torahack',
+};
+console.log({ ...payload });
